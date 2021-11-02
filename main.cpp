01cba893d1de4f31b3569634e34654c6406290f3
andrey-avdeev-gh <jamesstoun@gmail.com>
2021-11-02 15:27:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,54 +7,66 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
-void task_1(cv::Mat image);
-
 int main()
 {
+	cv::Mat image;
+
+
 	std::cout << std::endl << "Enter full name of the file: " << std::endl;
 	std::string imagename = "";
 	std::cin >> imagename;
 
 	try
 	{
-		cv::Mat img = cv::imread(cv::samples::findFile(imagename), cv::IMREAD_COLOR);
-		if (img.empty())
+		image = cv::imread(cv::samples::findFile(imagename), cv::IMREAD_COLOR);
+		if (image.empty())
 		{
 			std::cout << "Image cannot be loaded..!!" << std::endl;
 			exit(0);
 		}
-
-		task_1(img);
 	}
 	catch (cv::Exception)
 	{
 		std::cout << "Image cannot be loaded..!!" << std::endl;
 		exit(0);
 	}
-
-
-}
-
-
-void task_1(cv::Mat image)
-{
+	//------------------------------Task 1------------------------------------------
 	cv::CascadeClassifier faceDetection;
 	if (!faceDetection.load("C:\\Users\\A_And\\source\\repos\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_default.xml"))
 		std::cout << "XML file not found";
-
+	cv::Rect roi_c;
 	std::vector<cv::Rect> faces;
 	faceDetection.detectMultiScale(image, faces);
 
 	for (int i = 0; i < faces.size(); i++) {
-		cv::Point pt1(faces[i].x, faces[i].y);
-		cv::Point pt2((faces[i].x + faces[i].height + faces[i].height / 10),(faces[i].y + faces[i].width + faces[i].width / 10));
-		cv::rectangle(image, pt1, pt2, cv::Scalar(0, 255, 0), 2,8,0);
+		roi_c.x = faces[i].x;
+		roi_c.y = faces[i].y;
+		roi_c.width = (faces[i].width);
+		roi_c.height = (faces[i].height);
+		//-----------------------Green Rectangle--------------------------------------
+		//cv::Point pt1(faces[i].x, faces[i].y);
+		//cv::Point pt2((faces[i].x + faces[i].height), (faces[i].y + faces[i].width));
+		//cv::rectangle(image, pt1, pt2, cv::Scalar(0, 255, 0), 2, 8, 0);
+
 	}
+	//------------------------------Task 2------------------------------------------
+	const cv::Rect roi(roi_c.x, roi_c.y, roi_c.width + roi_c.width / 10, roi_c.height + roi_c.height / 10);
+	image = image(roi).clone();
+	//------------------------------Task 3------------------------------------------
+	
+	//------------------------------Task 4------------------------------------------
+	//------------------------------Task 5------------------------------------------
+	//------------------------------Task 6------------------------------------------
+	//------------------------------Task 7------------------------------------------
+	//------------------------------Task 8------------------------------------------
+	//------------------------------Task 9------------------------------------------
 
-	cv::namedWindow("original", cv::WINDOW_AUTOSIZE);
 
-	imshow("original", image);
+	cv::namedWindow("Picture", cv::WINDOW_AUTOSIZE);
+
+	imshow("Picture", image);
 
 	cv::waitKey(0);
 	cv::destroyAllWindows();
+
 }
